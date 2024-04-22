30171f01d90e0d459ae285ad139be725fa970513
spicypunch <miniguni66@naver.com>
2024-04-22 23:37:53 +0900

    Feature: DataStore Hilt 적용

---
+++ app/src/main/java/kr/jm/salmal_android/di/DataStoreModule.kt
@@ -0,0 +1,12 @@
+package kr.jm.salmal_android.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+
+}
