a0dbc24e05ff9bde948c5e63de61c9cc503de5c7
spicypunch <miniguni66@naver.com>
2024-05-06 00:33:02 +0900

    Feature: 백업

--- app/src/main/java/kr/jm/salmal_android/screen/home/HomeScreen.kt
+++ app/src/main/java/kr/jm/salmal_android/screen/home/HomeScreen.kt
@@ -8,22 +8,49 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.launch
+import kr.jm.salmal_android.utils.Utils
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU) {
-        val list = arrayListOf(Manifest.permission.POST_NOTIFICATIONS)
-    }
+    val permissionsList: Array<String> =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arrayOf(
+                Manifest.permission.POST_NOTIFICATIONS,
+            )
+        }
+
+        Utils.requestPermissions(
+            list = permissionsList,
+            useDeniedMessage = false,
+            onGranted = {
+                scope.launch {
+                    snackbarHostState.showSnackbar("알림이 허용되었습니다.")
+                }
+            },
+            onDenied = {
+                scope.launch {
+                    snackbarHostState.showSnackbar("알림이 거부되었습니다.")
+                }
+            }
+        )
+
 
     Scaffold(
         topBar = {
